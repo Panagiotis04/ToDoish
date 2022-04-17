66da2df3c809447fd6dfa2bde27e7477d20c8bac
Panagiotis Chatziioannou <panagiotischatziioannou0@gmail.com>
2022-04-18 00:03:04 +0200

    Created base for the popup screen for creating a new task

--- App.js
+++ App.js
@@ -9,20 +9,40 @@ import {
   TextInput,
   TouchableOpacity,
   ImageBackground,
+  Keyboard,
+  Image,
+  Modal,
   SafeAreaView } from 'react-native';
 
 import React, {useState} from 'react';
 import Task from './components/Task'
 
+const CreateTaskPopup = ({visible, childre}) => {
+  const [showModal, setShowModal] = useState(visible)
+  return <Modal transparent visible={true}>
+    <View style={styles.createTaskBackground}>
+      <View style={styles.taskContainer}></View>
+    </View>
+  </Modal>
+};
+
 function App(props) {
   const [task, setTask] = useState();
   const [taskItems, setTaskItems] = useState([]);
+  const [visible, setVisible] = useState(false);
+
 
   const haddleAddTask = () => {
-    setTaskItems([...taskItems, task])
-    setTask(null);
-    console.log(taskItems)
+    // Keyboard.dismiss()
+    // setTaskItems([...taskItems, task])
+    // setTask(null);
   }  
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
 
   return (
     <View style={styles.container}>
@@ -33,13 +53,26 @@ function App(props) {
           {/* Tasks */}
           <View style={styles.item}>
             {
-              taskItems.map((item) => {
-                <Task text={item} /> 
+              taskItems.map((item, index) => {
+                return (
+                  <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                    <Task text={item} /> 
+                  </TouchableOpacity>
+                )
               })
             }
           </View>
         </View>
       </View>
+
+      <CreateTaskPopup visible={visible}>
+        <View style={{alignItems: 'center}'}}>
+          <View style={styles.header}>
+            <Image source={require('./assets/icon.png')} style={{height: 20, width: 20}} />
+          </View>
+        </View>
+      </CreateTaskPopup>
+      <Button title='Popup' onPress={() => setVisible(true)} />
 
       <KeyboardAvoidingView
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
@@ -103,5 +136,27 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     borderColor: '#C0C0C0',
     borderWidth: 1,
-  }
+  },
+  createTaskBackground: {
+    flex: 1,
+    backgroundColor: 'gray',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: '5%',
+    paddingVertical: '50%',
+  },
+  taskContainer: {
+    width: '80%',
+    backgroundColor: 'white',
+    paddingHorizontal: 40,
+    paddingVertical: 50,
+    borderRadius: 20,
+    elevation: 20,
+    alignItems: 'center'
+  },
+  header: {
+    width: '100%',
+    height: 40,
+    alignItems: 'center',
+  },
 });
